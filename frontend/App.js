ddf44fb1aab11e9d8700a69425fe31a6788deff8
Zhi Wei <tanzhiwei7@gmail.com>
2023-06-19 15:27:43 +0800

    App bar update

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import 'react-native-gesture-handler';
-import HomeScreen from './screens/Home';
+import React from "react";
+import "react-native-gesture-handler";
+import HomeScreen from "./screens/Home";
+import CheckIn from "./screens/CheckIn";
+
 /* import HomePage from './screens/Home';
 import RegisterPage from './screens/Register'; */
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
@@ -12,13 +14,16 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-       {/*  <Stack.Screen name="Register" component={RegisterPage} options={{ title: 'Register' }} />
+        <Stack.Screen
+          name="CheckIn"
+          component={CheckIn}
+          options={{ title: "Check In Page" }}
+        />
+        {/*  <Stack.Screen name="Register" component={RegisterPage} options={{ title: 'Register' }} />
         <Stack.Screen name="Home" component={HomePage} options={{ title: 'Home' }} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
 
 export default App;
